701b003056115e1bf45ac74659b05ca3a900764c
dmitri9149 <dmitri.as.name@gmail.com>
2020-12-13 17:59:11 +0200

    initial commit

---
+++ ex_3.3/builder.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+error_exit() {
+    echo "$1" 1>&2
+    exit 1
+}
+
+
+if [ -z "$GIT_URL" ]; then
+  echo "GIT_URL env variable not set. Exiting."
+  exit 1
+fi
+
+if [ -z "$CONTAINER_NAME" ]; then
+  echo "CONTAINER NAME env variable not set. Exiting."
+  exit 1
+fi
+
+git clone $GIT_URL repo || error_exit "Unable to clone $GIT_URL. Exiting."
+cd repo || error_exit "Can not change directory! Exiting"
+
+if [ ! -f "Dockerfile" ]; then
+  echo "Dockerfile not found. Exiting."
+  exit 1
+fi
+
+docker build -t $CONTAINER_NAME . || error_exit "Unable to build image. Exiting"
+
+if [ "$PUSH_TO_DOCKER" = true ]; then
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+  docker push $CONTAINER_NAME
+fi
